5f341c6e3a8f30ce36927096d7d93095948f5721
Yechan Yun <yyc837021@gmail.com>
2023-07-07 14:16:53 +0900

    ~12th chapter done

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -1,3 +1,6 @@
+import mongodb from 'mongodb';
+const ObjectId = mongodb.ObjectId;
+
 let movies;
 
 export default class MoviesDAO {
@@ -38,6 +41,40 @@ export default class MoviesDAO {
         } catch(e) {
             console.error(`Unable to issue find command, ${e}`);
             return { moviesList: [], totalNumMovies: 0};
+        }
+    }
+
+    static async getMovieById(id){
+        try{
+            return await movies.aggregate([
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    }
+                },
+                { $lookup:
+                    {
+                        from: 'reviews',
+                        localField: '_id',
+                        foreignField: 'movie_id',
+                        as: 'reviews',
+                    }
+                }
+            ]).next()
+        }catch(e){
+            console.error(`something went wrong in getMovieById: ${e}`);
+            throw e;
+        }
+    }
+
+    static async getRatings(){
+        let ratings = [];
+        try{
+            ratings = await movies.distinct("rated");
+            return ratings;
+        }catch(e){
+            console.error(`unable to get ratings, ${e}`);
+            return ratings;
         }
     }
 }
